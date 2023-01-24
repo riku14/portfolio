b00980d408672e449b74f05d5bc8ec056c86caf3
riku14 <115448381+riku14@users.noreply.github.com>
2023-01-25 07:13:59 +0900

    資材の編集画面を実装 更新、削除昨日の追加

--- InventControl-1/src/main/java/com/example/demo/controller/SignupController.java
+++ InventControl-1/src/main/java/com/example/demo/controller/SignupController.java
@@ -1,12 +1,18 @@
 package com.example.demo.controller;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.domain.materials.model.Materials;
+import com.example.demo.domain.materials.service.MaterialsService;
 import com.example.demo.form.SignupForm;
 
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +21,12 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/materials")
 @Slf4j
 public class SignupController {
+	
+	@Autowired
+	private MaterialsService materialsService;
+	
+	@Autowired
+	private ModelMapper modelMapper;
 	
 	//資材登録画面を表示
 	@GetMapping("/signup")
@@ -25,16 +37,28 @@ public class SignupController {
 	}
 	
 	
-	//資材登録処
+	//資材登録処理
 	@PostMapping("/signup")
-	public String postSignup(@ModelAttribute SignupForm form) {
+	public String postSignup(Model model, @ModelAttribute @Validated SignupForm form, BindingResult bindingResult) {
 		
-		
+		//入力チェック結果
+		if(bindingResult.hasErrors()) {
+			//エラー発生したら資材登録画面に戻る
+			return getSignup(model, form);
+		}
 		
 		log.info(form.toString());
 		
+		//formをMaterialsクラスに変換（modelMapperでformのフィールドをコピー）
+		//他の画面からもサービスを利用しやすい、formの変更がMaterialsServiceに影響しない
+		Materials materials = modelMapper.map(form, Materials.class);  
+		
+		
+		//資材を登録
+		materialsService.signup(materials);  //資材登録処理の呼び出し
+		
 		//資材詳細一覧画面に遷移
-		return "redirect:/list";
+		return "redirect:/materials/list";
 	}
 
 }
--- InventControl-1/src/main/java/com/example/demo/form/SignupForm.java
+++ InventControl-1/src/main/java/com/example/demo/form/SignupForm.java
@@ -1,18 +1,28 @@
 package com.example.demo.form;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import org.springframework.format.annotation.NumberFormat;
 
 import lombok.Data;
 
 @Data
 public class SignupForm {
+	
+	@NotBlank
 	private String name;
+	
+	@NotNull
 	private Integer tekisei;
+	@NotNull
 	private Integer zaiko;
 	
+	@NotNull
 	@NumberFormat(pattern = "#,###")
 	private Integer price;
 	
+	@NotNull
 	private String detail;
 
 }
